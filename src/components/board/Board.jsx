b581482b786385823ae5bb7d5536678f1d15d7f0
SDvision <sanchit.dahatonde@gmail.com>
2021-06-04 13:05:10 +0530

    Board and Player completed

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import "./Board.css"
+import React, {Fragment} from 'react';
+import { connect } from 'react-redux'
 
 import Square from './square/Square.jsx'
 
 function Board(props){
-    //TODO: do it with redux
-    const board = [1,2,3,4,5,6,7,8,9]
+    const board = props.board
+    const players = props.players
+    
     return (
-        <div id="board" className="d-flex flex-wrap">
-            {
-                board.map((ele, i)=> <Square key={i} seq={ele}/>)
-            }
-        </div>
+        <Fragment>
+            <div id="board" className="d-flex flex-wrap">
+                {
+                    board.map((symbol, i)=> <Square key={i} index={i} symbol={symbol} />)
+                }
+            </div>
+            <div className="mt-4">
+                <div className="col">
+                    <p><strong>Player 1</strong>: {players.p1}</p>
+                    <p><strong>Player 2</strong>: {players.p2}</p>
+                    <p><strong>Current Turn</strong>: {players.turn === 'p1' ? 'Player 1' : 'Player 2'}</p>
+                </div>
+            </div>
+        </Fragment>
     )
 }
-export default Board;
+export default connect(({board, players}) => ({board, players}))(Board);
--- src/components/board/square/Square.jsx
+++ src/components/board/square/Square.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
 import Cross from './cross/Cross.jsx'
 import Zero from './zero/Zero.jsx'
+import { drawXAction, drawOAction } from '../../../actions/boardAction'
+import { toggleTurnAction } from '../../../actions/playerActions.jsx';
 function Square(props){
+    const { symbol, index, draw, players, board } = props
+    const disabled = symbol ? 'disabled' : ''
     return (
-        <div className="cell">
-            {props.seq%2===0?<Cross />:<Zero />} 
+        <div className={'cell ' + disabled} onClick={() => draw(board, players, index)} >
+            { symbol ? ((symbol === 'X')? <Cross /> : <Zero />): ''} 
         </div>
     )
 }
-export default Square;
+
+const mapStateToProps = ({ board, players }) => ({ board, players })
+
+const mapDispatchToProps = dispatch => ({
+    draw: (board, players, cellIndex) => {
+        if(!board[cellIndex]) {
+            if(players[players.turn] === 'X'){
+            dispatch(drawXAction(cellIndex))
+            } else {
+            dispatch(drawOAction(cellIndex)) 
+            }
+            dispatch(toggleTurnAction())
+        }
+    }
+})
+export default connect(mapStateToProps, mapDispatchToProps)(Square);
